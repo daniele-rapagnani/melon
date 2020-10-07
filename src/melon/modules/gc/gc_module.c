cb507ad47210c8c6790330b6669d1983b648b0d6
Daniele Rapagnani <daniele.rapagnani@barrysoft.it>
2020-10-08 00:03:11 +0200

    - :loud_sound: Adds some more logging when the GC is in debug mode - :bug: Fixes a nasty GC bug caused by a missing write barrier on STOREUPVAL. - :sparkles: Adds an 'info' function to the GC module which returns a string representing the object's GC status.

--- src/melon/modules/gc/gc_module.c
+++ src/melon/modules/gc/gc_module.c
@@ -1,6 +1,8 @@
 #include "melon/modules/gc/gc_module.h"
 #include "melon/modules/modules.h"
 #include "melon/core/gc.h"
+#include "melon/core/utils.h"
+#include "melon/core/tstring.h"
 
 #include <stdlib.h>
 #include <assert.h>
@@ -15,9 +17,44 @@ static TByte triggerFunc(VM* vm)
     return 1;
 }
 
+static TByte infoFunc(VM* vm)
+{
+    melM_arg(vm, obj, MELON_TYPE_NONE, 0);
+
+    if (!melM_isGCItem(obj))
+    {
+        melM_vstackPushNull(&vm->stack);
+        return 1;
+    }
+
+    struct StrFormat sf;
+    memset(&sf, 0, sizeof(struct StrFormat));
+
+    melStringFmtUtils(
+        &sf,
+        "(%p) age = %d, old = %s, color = %d, dark = %s",
+        obj->pack.obj,
+        melM_getAgeGCItem(&vm->gc, obj->pack.obj),
+        melM_isOldGCItem(&vm->gc, obj->pack.obj) ? "yes" : "no",
+        melM_getColorGCItem(&vm->gc, obj->pack.obj),
+        melM_isDarkGCItem(&vm->gc, obj->pack.obj) ? "yes" : "no"
+    );
+
+    Value val;
+    val.pack.obj = melNewString(vm, sf.buffer, sf.used);
+    val.type = val.pack.obj->type;
+
+    melM_stackPush(&vm->stack, &val);
+
+    melStringFmtFreeUtils(&sf);
+
+    return 1;
+}
+
 static const ModuleFunction funcs[] = {
     // name, args, locals, func
     { "trigger", 0, 0, &triggerFunc },
+    { "info", 1, 0, &infoFunc },
     { NULL, 0, 0, NULL }
 };
 
